39f34be24721a34610256988151cf23cc1670f3a
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-03-17 14:30:55 +0300

    added the grading system code

---
+++ Grading/grading.js
@@ -0,0 +1,34 @@
+ //student grading function
+ function gradeStudent(marks){
+    //checking where the marks lie
+    //range 80-100
+    if(marks > 79 && marks <= 100){
+        grade = "A";
+    }
+    //range 60-79
+    else if(marks <= 79 && marks >= 60){
+        grade = "B";
+    }
+    //range 49-59
+    else if(marks <= 59 && marks >= 49){
+        grade = "c";
+    }
+    //range 40-48
+    else if(marks <= 48 && marks >= 40){
+        grade = "D";
+    }
+    //less than or equal to 39
+    else if(marks < 40){
+        grade = "E";
+    }
+    // any value outside the range of 0 - 100
+    else {
+        grade = "Invalid grade input"
+    }
+    return grade;
+ }
+//prompt user to enter grade
+let score= prompt("Enter your marks: ")
+// calling the function
+gradeStudent(score)
+
